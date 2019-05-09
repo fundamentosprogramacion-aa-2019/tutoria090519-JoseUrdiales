92cd7639664a14b832f8da15da4d6814e15083b0
JoseUrdiales <jose-urdiales@hotmail.com>
2019-05-09 17:46:15 -0500

    Parte 2

---
+++ TutoriaClase6/src/menejosswitch/UsoSwitch3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menejosswitch;
+
+/**
+ *
+ * @author Jose Luis
+ */
+public class UsoSwitch3 {
+    public static void main(String[] args) {
+        String nombre = "esmeraldAS";
+        char valor = nombre.charAt(0);
+        
+        switch(valor){
+            case 'a':
+            case 'A':
+                System.out.printf("Nombre con inicial %s de %s\n", valor, 
+                        nombre.toUpperCase());
+                break;
+            case 'e':
+                System.out.printf("Nombre con inicial %s de %s\n", valor, 
+                        nombre.toLowerCase());
+                break;
+            default:
+                System.out.printf("Ninguna de las anteriores");
+        }
+    }
+}
